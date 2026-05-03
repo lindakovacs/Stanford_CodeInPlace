27226400c007c0d5db9626645440900807a6b805
Linda Kovacs <lindaprovenza@yahoo.com>
2026-05-03 15:36:57 -0400

    Add Lesson 2

---
+++ Lesson2/Square.py
@@ -0,0 +1,25 @@
+"""
+This is a worked example. This code is starter code; you should edit and run it to 
+solve the problem. You can click the blue show solution button on the left to see 
+the answer if you get too stuck or want to check your work!
+"""
+
+from karel.stanfordkarel import *
+
+def main():
+    """ Makes Karel place beepers in a square (4 beepers total) and end in the same position Karel starts in. """
+    # If we consider the task carefully, we need to repeat several actions
+    # Since we are repeating a these actions a known number of times, we use a for-loop
+    for i in range(4):
+        # Put beeper
+        put_beeper()
+
+        # Makes us move in a square motion when we do it four times
+        move()
+        turn_left()
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
