dfdb506168de722f9589a88eed96b91301b3838a
Linda Kovacs <lindaprovenza@yahoo.com>
2026-05-16 22:43:43 -0400

    Add Lesson 10 - Complete

---
+++ Lesson10/DrawFlag.py
@@ -0,0 +1,14 @@
+from graphics import Canvas
+import random
+
+CANVAS_WIDTH = 450
+CANVAS_HEIGHT = 300
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    
+    # Draw a red rectangle covering the top half of the canvas
+    canvas.create_rectangle(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT // 2, 'red')
+
+if __name__ == '__main__':
+    main()
